13d645eba29b7de6e72aa2d10f771663c79c7147
Chungmin <kws.stupid@gmail.com>
2024-08-10 14:13:28 +0700

    kotlin

---
+++ backend/billsplit/src/main/kotlin/com/demo/billsplit/BillsplitApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.billsplit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BillsplitApplication
+
+fun main(args: Array<String>) {
+	runApplication<BillsplitApplication>(*args)
+}
---
+++ backend/billsplit/src/test/kotlin/com/demo/billsplit/BillsplitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.billsplit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BillsplitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
