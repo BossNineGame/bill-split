0d76b93f01d2757920365aa486487d223badcd5e
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2024-08-14 06:06:16 +0700

    disable autoComplete

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const Input = (props: React.ComponentProps<"input">) => {
+  const [isFocused, setIsFocused] = useState(false);
   return (
     <input
       {...props}
       type={props.type || "text"}
       size={props.size || 10}
-      autoComplete="don't"
+      autoComplete="off"
       className={twMerge(
         "p-2 w-full box-border border-slate-700 focus:border-slate-300 border-0 bg-transparent text-white border-b-2 outline-none",
         props.className
       )}
+      readOnly={!isFocused}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
     />
   );
 };
