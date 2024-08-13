03e0b3a6a2d0ce2485dec6e9fa70a0e228789f10
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2024-08-14 06:02:31 +0700

    turn off autocomplete

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -6,7 +6,7 @@ const Input = (props: React.ComponentProps<"input">) => {
       {...props}
       type={props.type || "text"}
       size={props.size || 10}
-      autoComplete="off"
+      autoComplete="don't"
       className={twMerge(
         "p-2 w-full box-border border-slate-700 focus:border-slate-300 border-0 bg-transparent text-white border-b-2 outline-none",
         props.className
