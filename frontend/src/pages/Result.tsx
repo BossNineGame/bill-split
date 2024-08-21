6bba87e0ebd1396014bee795d7a474f8855e9b05
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2024-08-21 13:32:20 +0700

    exclude friends with no bills

--- frontend/src/pages/Result.tsx
+++ frontend/src/pages/Result.tsx
@@ -44,96 +44,98 @@ const Result = () => {
         </button>
       </div>
       <div className="grid md:grid-cols-3 gap-4 w-full">
-        {Array.from(friendToBills).map(([friend, bills]) => {
-          const itemPricePairs = Array.from(bills).map((billKey) => {
-            const item = items.get(billKey);
-            const friends = billToFriends.get(billKey);
-            if (!item || !friends) return [];
-            return [
-              billKey,
-              ((item.price * item.quantity) / friends.size).toFixed(2),
-            ];
-          });
+        {Array.from(friendToBills)
+          .filter(([, bills]) => bills.size > 0)
+          .map(([friend, bills]) => {
+            const itemPricePairs = Array.from(bills).map((billKey) => {
+              const item = items.get(billKey);
+              const friends = billToFriends.get(billKey);
+              if (!item || !friends) return [];
+              return [
+                billKey,
+                ((item.price * item.quantity) / friends.size).toFixed(2),
+              ];
+            });
 
-          const adjustmentPricePairs = Array.from(adjustments).map(
-            ([adjustmentKey, adjustment]) => [
-              adjustment,
-              itemPricePairs
-                .reduce(
-                  (acc, [billKey, price]) =>
-                    adjustmentToBills.get(adjustmentKey)?.has(billKey)
-                      ? acc +
-                        (adjustment.percentage * parseFloat(price)) / 100.0
-                      : acc,
-                  0
-                )
-                .toFixed(2),
-            ]
-          ) as [Adjustment, string][];
+            const adjustmentPricePairs = Array.from(adjustments).map(
+              ([adjustmentKey, adjustment]) => [
+                adjustment,
+                itemPricePairs
+                  .reduce(
+                    (acc, [billKey, price]) =>
+                      adjustmentToBills.get(adjustmentKey)?.has(billKey)
+                        ? acc +
+                          (adjustment.percentage * parseFloat(price)) / 100.0
+                        : acc,
+                    0
+                  )
+                  .toFixed(2),
+              ]
+            ) as [Adjustment, string][];
 
-          // const adjustmentPricePairsAcc = adjustmentPricePairs.map(
-          //   ([adjustment, price], idx) => [
-          //     adjustment,
-          //     (
-          //       (idx > 0
-          //         ? (parseFloat(adjustmentPricePairs[idx - 1][1]) *
-          //             adjustment.percentage) /
-          //           100.0
-          //         : 0) + parseFloat(price)
-          //     ).toFixed(2),
-          //   ]
-          // );
+            // const adjustmentPricePairsAcc = adjustmentPricePairs.map(
+            //   ([adjustment, price], idx) => [
+            //     adjustment,
+            //     (
+            //       (idx > 0
+            //         ? (parseFloat(adjustmentPricePairs[idx - 1][1]) *
+            //             adjustment.percentage) /
+            //           100.0
+            //         : 0) + parseFloat(price)
+            //     ).toFixed(2),
+            //   ]
+            // );
 
-          const totalPrice = (
-            itemPricePairs.reduce(
-              (acc, [, price]) => acc + parseFloat(price),
-              0
-            ) +
-            adjustmentPricePairs.reduce(
-              (acc, [, price]) => acc + parseFloat(price),
-              0
-            )
-          ).toFixed(2);
-          return (
-            <div
-              className="grid grid-flow-row auto-rows-[min-content_auto_min-content] w-full gap-4 divide-slate-500 p-4 rounded-lg bg-slate-600/20 shadow-slate-700 shadow-inner backdrop-blur-sm"
-              key={friend}
-            >
-              <h2 className="text-lg text-white">{friend}</h2>
-              <div className="grid grid-flow-row auto-rows-min gap-1">
-                {itemPricePairs.map(([billKey, price]) => (
-                  <div
-                    className="grid grid-flow-col gap-6 auto-cols-[auto_min-content] text-slate-400 text-sm"
-                    key={billKey}
-                  >
-                    <p>{items.get(billKey)!.name}</p>
-                    <p>{price}</p>
+            const totalPrice = (
+              itemPricePairs.reduce(
+                (acc, [, price]) => acc + parseFloat(price),
+                0
+              ) +
+              adjustmentPricePairs.reduce(
+                (acc, [, price]) => acc + parseFloat(price),
+                0
+              )
+            ).toFixed(2);
+            return (
+              <div
+                className="grid grid-flow-row auto-rows-[min-content_auto_min-content] w-full gap-4 divide-slate-500 p-4 rounded-lg bg-slate-600/20 shadow-slate-700 shadow-inner backdrop-blur-sm"
+                key={friend}
+              >
+                <h2 className="text-lg text-white">{friend}</h2>
+                <div className="grid grid-flow-row auto-rows-min gap-1">
+                  {itemPricePairs.map(([billKey, price]) => (
+                    <div
+                      className="grid grid-flow-col gap-6 auto-cols-[auto_min-content] text-slate-400 text-sm"
+                      key={billKey}
+                    >
+                      <p>{items.get(billKey)!.name}</p>
+                      <p>{price}</p>
+                    </div>
+                  ))}
+                </div>
+                <div className="grid grid-flow-row auto-rows-min gap-1">
+                  {adjustmentPricePairs.map(
+                    ([adjustment, price]) =>
+                      parseFloat(price) !== 0 && (
+                        <div
+                          className="grid grid-flow-col gap-6 auto-cols-[auto_min-content] text-slate-400 text-sm"
+                          key={adjustment.name}
+                        >
+                          <p>{`${adjustment.name} ${adjustment.percentage}%`}</p>
+                          <p>{price}</p>
+                        </div>
+                      )
+                  )}
+                </div>
+                <div>
+                  <div className="grid grid-flow-col auto-cols-[auto_min-content] text-base text-white">
+                    <p> Total </p>
+                    <p> {totalPrice} </p>
                   </div>
-                ))}
-              </div>
-              <div className="grid grid-flow-row auto-rows-min gap-1">
-                {adjustmentPricePairs.map(
-                  ([adjustment, price]) =>
-                    parseFloat(price) !== 0 && (
-                      <div
-                        className="grid grid-flow-col gap-6 auto-cols-[auto_min-content] text-slate-400 text-sm"
-                        key={adjustment.name}
-                      >
-                        <p>{`${adjustment.name} ${adjustment.percentage}%`}</p>
-                        <p>{price}</p>
-                      </div>
-                    )
-                )}
-              </div>
-              <div>
-                <div className="grid grid-flow-col auto-cols-[auto_min-content] text-base text-white">
-                  <p> Total </p>
-                  <p> {totalPrice} </p>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
